65dbf9c65936ff9c38aa15191adccecb02951145
matevzkusterle <82824561+matevzkusterle@users.noreply.github.com>
2023-12-20 12:26:42 +0100

    Create algoritem_dimenzije.py

---
+++ algoritem_dimenzije.py
@@ -0,0 +1,26 @@
+from sage.numerical.mip import MixedIntegerLinearProgram
+
+def weak_edge_k_metric_dimension_ILP(graph, k):
+    # Create an ILP instance
+    p = MixedIntegerLinearProgram(maximization=False)
+
+    # Define variables
+    x = p.new_variable(binary=True)
+    vertices = graph.vertices()
+
+    # Objective function: Minimize the sum of xi variables
+    p.set_objective(p.sum(x[vertex] for vertex in vertices))
+
+    # Constraint:
+    for ea, eb in graph.edges(labels=False):
+    p.add_constraint(sum(abs(graph.distance(ea, vertex) - graph.distance(eb, vertex)) * x[vertex] for vertex in vertices) >= k)
+
+    # Solve the ILP
+    p.solve()
+
+    # Extract the solution
+    solution = p.get_values(x)
+
+    # Return the cardinality of the selected set
+    dimension = sum(1 for vertex in vertices if solution[vertex] == 1)
+    return dimension
